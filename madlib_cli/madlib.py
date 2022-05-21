548c280bb6c7e59372f279aabfb8ad02de464c19
AoifeWM <aoifewmulligan@gmail.com>
2022-05-21 15:38:44 -0700

    more initial setup

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,18 @@
+
+def read_template(filepath):
+    print(filepath)
+    with open(filepath) as template:
+        stripped = template.read()
+    print(stripped)
+    return stripped
+
+
+def parse_template():
+    b = 1
+
+
+def merge():
+    c = 1
+
+
+print(read_template("assets/dark_and_stormy_night_template.txt"))
